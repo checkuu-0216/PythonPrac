b690f7969157e60a98049d08f6eaa5e9ceba3ed0
checkuu-0216 <corb37@gmail.com>
2025-03-27 16:19:06 +0900

    랜덤값 생성성

---
+++ practice_3.3.py
@@ -0,0 +1,32 @@
+from random import *
+# 랜덤 수 뽑기기
+# print (random()) # 0.0 ~ 1.0 미만의 임의의 값 생성
+# print(random() * 10) # 0.0 ~ 10.0 값 생성
+# print(int(random() * 10)) # 0~ 10 미만의 값 생성
+# print(int(random() * 10)) # 0~ 10 미만의 값 생성
+# print(int(random() * 10)) # 0~ 10 미만의 값 생성
+# print(int(random() * 10) + 1) # 1 ~ 10 미만의 값 생성
+# print(int(random() * 10) + 1) # 1 ~ 10 미만의 값 생성
+# print(int(random() * 10) + 1) # 1 ~ 10 미만의 값 생성
+# print(int(random() * 10) + 1) # 1 ~ 10 미만의 값 생성
+
+
+# #로또 번호 생성기기
+# print(int(random() * 45) + 1) # 1 ~ 45 이하의 임의의 값 생성
+# print(int(random() * 45) + 1) # 1 ~ 45 이하의 임의의 값 생성
+# print(int(random() * 45) + 1) # 1 ~ 45 이하의 임의의 값 생성
+# print(int(random() * 45) + 1) # 1 ~ 45 이하의 임의의 값 생성
+# print(int(random() * 45) + 1) # 1 ~ 45 이하의 임의의 값 생성
+# print(int(random() * 45) + 1) # 1 ~ 45 이하의 임의의의 값 생성
+
+# #로또 번호 생성기기
+# print(randrange(1,46)) # 1~ 46 미만의 임의의 값 생성
+# print(randrange(1,46)) # 1~ 46 미만의 임의의 값 생성
+# print(randrange(1,46)) # 1~ 46 미만의 임의의 값 생성
+# print(randrange(1,46)) # 1~ 46 미만의 임의의 값 생성
+# print(randrange(1,46)) # 1~ 46 미만의 임의의 값 생성
+# print(randrange(1,46)) # 1~ 46 미만의 임의의 값 생성
+
+print(randint(1,45))
+
+
