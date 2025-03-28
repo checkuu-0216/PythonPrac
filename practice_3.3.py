807f80a546d533e063248c2074b3ec00716484af
checkuu-0216 <corb37@gmail.com>
2025-03-28 17:34:46 +0900

    [Fix] 코드 수정

--- practice_3.3.py
+++ practice_3.3.py
@@ -1,4 +1,5 @@
 from random import *
+import random
 # 랜덤 수 뽑기기
 # print (random()) # 0.0 ~ 1.0 미만의 임의의 값 생성
 # print(random() * 10) # 0.0 ~ 10.0 값 생성
@@ -27,6 +28,10 @@ from random import *
 # print(randrange(1,46)) # 1~ 46 미만의 임의의 값 생성
 # print(randrange(1,46)) # 1~ 46 미만의 임의의 값 생성
 
-print(randint(1,45))
+#로또 번호 생성기
+date = random.sample(range(1,46) ,6)
+print(date)
+
+# print(randint(1,45))
 
 
