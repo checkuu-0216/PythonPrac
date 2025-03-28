e63859fda17999da05aefc0f614fcbf46f3c0c11
checkuu-0216 <corb37@gmail.com>
2025-03-29 00:04:47 +0900

    [Add] 다중상속에서의 super 문제점

--- practice_9.py
+++ practice_9.py
@@ -70,18 +70,9 @@ class FlyableAttackUnit(AttackUnit, Flyable):
 
 class BuildingUnit(Unit):
     def __init__(self,name,hp,location):
-        pass
-
-supply_depot = BuildingUnit("서플라이 디폿", 500, "7시")
-
-def game_start():
-    print("[알림] 새로운 게임을 시작합니다.")
-
-def game_over():
-    pass
-
-game_start()
-game_over()
+        # Unit.__init__(self,name,hp,0)
+        super().__init__(name,hp,0)
+        self.location = location
 
 # marine1 = Unit("마린", 40 ,5)
 # marine2 = Unit("마린", 40 ,5)
