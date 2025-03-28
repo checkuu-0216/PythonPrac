73d91f6a18e4395b6b2f8a815c834ca47234e9b3
checkuu-0216 <corb37@gmail.com>
2025-03-28 23:58:03 +0900

    [Add]상속, 다중상속, 오버로딩, pass

--- practice_9.py
+++ practice_9.py
@@ -29,14 +29,59 @@
 # __init__ 생성자
 #멤버변수 클래스내에서 정의된 변수 self.name
 class Unit:
-    def __init__(self, name, hp, damage):
+    def __init__(self, name, hp, speed):
         self.name = name
         self.hp = hp
-        self.damage = damage
-        print("{0} 유닛이 생성되었습니다.".format(self.name))
-        print("체력 {0} , 공격력 {1}".format(self.hp,self.damage))
+        self.speed = speed
 
+    def move(self, location):
+        print("[지상 유닛 이동]")
+        print("{0} : {1} 방향으로 이동합니다. [속도 : {2}]".format(self.name,location,self.speed))
 
+class AttackUnit(Unit):
+    def __init__(self, name, hp, speed, damage):
+        Unit.__init__(self,name,hp,speed) # 상속 -> 클래스안에 중복된 부분을 클래스를 상속받아와서 쓴다. Unit이 부모
+        self.damage = damage   
+    def attack(self, location): #메서드
+        print("{0} : {1} 방향으로 적군을 공격합니다. [공격력 : {2}]".format(self.name,location,self.damage))
+
+    def damaged(self, damage): #메서드
+        print("{0} : {1} 데미지를 입었습니다.".format(self.name,damage))
+        self.hp -= damage
+        print("{0} : 현재체력은 {1} 입니다.".format(self.name,self.hp))
+        if self.hp <= 0:
+            print("{0} : 파괴되었습니다.".format(self.name))
+
+class Flyable:
+    def __init__(self, flying_speed):
+        self.flying_speed = flying_speed
+
+    def fly(self, name, location):
+        print("{0} : {1} 방향으로 날아갑니다. [속도 : {2}]".format(name,location,self.flying_speed))
+
+class FlyableAttackUnit(AttackUnit, Flyable):
+    def __init__(self, name, hp, damage, flying_speed):
+        AttackUnit.__init__(self, name,hp,0,damage) #지상 스피드를 0으로 처리
+        Flyable.__init__(self, flying_speed)
+
+    def move(self, location):
+        print("[공중 유닛 이동]")
+        self.fly(self.name, location)
+
+class BuildingUnit(Unit):
+    def __init__(self,name,hp,location):
+        pass
+
+supply_depot = BuildingUnit("서플라이 디폿", 500, "7시")
+
+def game_start():
+    print("[알림] 새로운 게임을 시작합니다.")
+
+def game_over():
+    pass
+
+game_start()
+game_over()
 
 # marine1 = Unit("마린", 40 ,5)
 # marine2 = Unit("마린", 40 ,5)
@@ -50,24 +95,18 @@ class Unit:
 # if wraith2.clocking == True:
 #     print("{0} 는 현재 클로킹 상태 입니다.".format(wraith2.name))
 
-#메서드 
-class AttackUnit:
-    def __init__(self, name, hp, damage):
-        self.name = name
-        self.hp = hp
-        self.damage = damage   
-    def attack(self, location):
-        print("{0} : {1} 방향으로 적군을 공격합니다. [공격력 : {2}]".format(self.name,location,self.damage))
+# firebat1 = AttackUnit("파이어뱃", 50, 16)
+# firebat1.attack("5시")
 
-    def damaged(self, damage):
-        print("{0} : {1} 데미지를 입었습니다.".format(self.name,damage))
-        self.hp -= damage
-        print("{0} : 현재체력은 {1} 입니다.".format(self.name,self.hp))
-        if self.hp <= 0:
-            print("{0} : 파괴되었습니다.".format(self.name))
+# firebat1.damaged(25)
+# firebat1.damaged(25)
 
-firebat1 = AttackUnit("파이어뱃", 50, 16)
-firebat1.attack("5시")
+# valkyrie = FlyableAttackUnit("발키리", 200, 6, 5)
+# valkyrie.fly(valkyrie.name, "3시")
 
-firebat1.damaged(25)
-firebat1.damaged(25)
+# vulture = AttackUnit("벌처", 80, 10, 20)
+# battlecruiser = FlyableAttackUnit("배틀크루저", 500, 25, 3)
+
+# vulture.move("11시")
+# # battlecruiser.fly(battlecruiser.name,"9시")
+# battlecruiser.move("9시")
